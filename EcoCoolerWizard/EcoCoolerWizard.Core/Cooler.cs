e310453241d2072b2a179c2dffa88ea24c08f8cb
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2022-01-31 17:48:49 +0100

    Rename project file

--- EcoCoolerWizard/EcoCoolerWizard.Core/Cooler.cs
+++ EcoCoolerWizard/EcoCoolerWizard.Core/Cooler.cs
@@ -6,8 +6,9 @@
         public double Height { get; set; }
         public int Rows { get; set; }
         public int Columns { get; set; }
-        public double HorizontalGap { get; set; }
-        public double VerticalGap { get; set; }
+        public double HorizontalGap { get; set; } = 5;
+        public double VerticalGap { get; set; } = 5;
+        public double CapRatio { get; set; } = 1;
         public double MarginLeft { get; set; }
         public double MarginRight { get; set; }
         public double MarginTop { get; set; }
--- EcoCoolerWizard/EcoCoolerWizard.Core/CoolerGenerator.cs
+++ EcoCoolerWizard/EcoCoolerWizard.Core/CoolerGenerator.cs
@@ -5,6 +5,7 @@
         private double ratio = 1;
         private double horizontalGap = 5;
         private double verticalGap = 5;
+
         public Cooler GenerateCooler(double windowWidth, double windowHeight)
         {
             var hDivRem = Math.DivRem((int)windowWidth * 1000, (int)horizontalGap * 1000);
@@ -22,6 +23,9 @@
                 Height = windowHeight,
                 Columns = columns,
                 Rows = rows,
+                CapRatio = ratio,
+                HorizontalGap = horizontalGap,
+                VerticalGap = verticalGap,
                 MarginLeft = (hMargin / 2),
                 MarginRight = (hMargin / 2),
                 MarginTop = (vMargin / 2),
