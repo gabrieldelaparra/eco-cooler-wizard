7c74007677875b09970a198bb7aed14a76d1b731
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2024-03-05 22:04:26 +0100

    Changes to the svg draw. Adds a multiplier factor and measurement lines.

--- EcoCoolerWizard/EcoCoolerWizard.Core/CoolerDrawer.cs
+++ EcoCoolerWizard/EcoCoolerWizard.Core/CoolerDrawer.cs
@@ -1,42 +1,102 @@
 ﻿using Svg;
-
 using System.Drawing;
+using Svg.Transforms;
 
+//Move this out from Core.
 namespace EcoCoolerWizard.Core
 {
     public class CoolerDrawer
     {
         public SvgDocument Draw(Cooler cooler)
         {
-            var width = (int)cooler.Width;
-            var height = (int)cooler.Height;
-            var margin = 2;
+            var width = (int) cooler.Width;
+            var height = (int) cooler.Height;
+            var margin = 4;
+            var m = 50;
 
             var svgDoc = new SvgDocument
             {
-                Width = width,
-                Height = height,
-                ViewBox = new SvgViewBox(-margin, -margin, width + margin, height + margin),
+                Width = width * m,
+                Height = height * m,
+                ViewBox = new SvgViewBox(
+                    -margin * m,
+                    -margin * m,
+                    width * m + margin * m,
+                    height * m + margin * m),
             };
 
-            var group = new SvgGroup() { };
-            svgDoc.Children.Add(group);
+            var circlesGroup = new SvgGroup() { };
+            svgDoc.Children.Add(circlesGroup);
 
-            for (int i = 0; i < cooler.Rows; i++)
+            for (int i = 0; i < cooler.Columns; i++)
             {
-                for (int j = 0; j < cooler.Columns; j++)
+                for (int j = 0; j < cooler.Rows; j++)
                 {
-                    group.Children.Add(new SvgCircle
+                    circlesGroup.Children.Add(new SvgCircle
                     {
-                        Radius = 1,
-                        CenterX = new SvgUnit((float)cooler.MarginLeft + (i * 5)),
-                        CenterY = new SvgUnit((float)cooler.MarginTop + (j * 5)),
-                        //Fill = new SvgColourServer(Color.Red),
+                        Radius = new SvgUnit((float) cooler.CapRatio * m),
+                        CenterX = new SvgUnit(
+                            (float) cooler.MarginLeft * m + (i * m * (float) cooler.HorizontalGap)),
+                        CenterY = new SvgUnit((float) cooler.MarginTop * m + (j * m * (float) cooler.VerticalGap)),
+                        Fill = new SvgColourServer(Color.Transparent),
                         Stroke = new SvgColourServer(Color.Black),
                         StrokeWidth = 1,
                     });
                 }
             }
+
+            svgDoc.Children.Add(new SvgRectangle()
+            {
+                Fill = new SvgColourServer(Color.Transparent),
+                Stroke = new SvgColourServer(Color.Black),
+                X = new SvgUnit(0),
+                Y = new SvgUnit(0),
+                Width = width * m,
+                Height = height * m
+            });
+
+            var measuresGroup = new SvgGroup() { };
+            svgDoc.Children.Add(measuresGroup);
+
+            var totalHorizontalLine = new SvgLine()
+            {
+                StartX = 0,
+                EndX = width * m,
+                StartY = (float) -(margin * m / 4 * 3),
+                EndY = (float) -(margin * m / 4 * 3),
+                Stroke = new SvgColourServer(Color.DarkBlue),
+                StrokeWidth = 1,
+            };
+            measuresGroup.Children.Add(totalHorizontalLine);
+            
+            var totalHorizontalMeasure = new SvgText($"{cooler.Width}")
+            {
+                X = new SvgUnitCollection() {new SvgUnit((totalHorizontalLine.StartX + totalHorizontalLine.EndX) / 2f)},
+                Y = new SvgUnitCollection() {new SvgUnit((totalHorizontalLine.StartY + totalHorizontalLine.EndY) / 2f)},
+                FontSize = new SvgUnit(SvgUnitType.Em, 2),
+                TextAnchor = SvgTextAnchor.Middle,
+            };
+            measuresGroup.Children.Add(totalHorizontalMeasure);
+
+            var totalVerticalLine = new SvgLine()
+            {
+                StartY = 0,
+                EndY = height * m,
+                StartX = (float) -(margin * m / 4 * 3),
+                EndX = (float) -(margin * m / 4 * 3),
+                Stroke = new SvgColourServer(Color.DarkBlue),
+                StrokeWidth = 1,
+            };
+            measuresGroup.Children.Add(totalVerticalLine);
+
+            var totalVerticalMeasure = new SvgText($"{cooler.Height}")
+            {
+                X = new SvgUnitCollection() {new SvgUnit((totalVerticalLine.StartX + totalVerticalLine.EndX) / 2f)},
+                Y = new SvgUnitCollection() {new SvgUnit((totalVerticalLine.StartY + totalVerticalLine.EndY) / 2f)},
+                TextAnchor = SvgTextAnchor.Middle,
+                FontSize = new SvgUnit(SvgUnitType.Em, 2),
+            };
+            measuresGroup.Children.Add(totalVerticalMeasure);
 
             return svgDoc;
 
